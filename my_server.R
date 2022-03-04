c294dcea8cccd3e88ecb25fa1b6acca9c338d2af
Erik Onsager <eonsag@uw.edu>
2022-03-04 10:55:19 -0800

    created interactive visualization and finished introduction

--- my_server.R
+++ my_server.R
@@ -1,12 +1,15 @@
 library(ggplot2)
 library(plotly)
 library(dplyr)
+library(tidyverse)
 
 server <- function(input, output) {
   
   output$calcValues <- renderText({
     
-    maxco2 <- climate_data %>% filter(year == 2020) %>% filter(co2 == max(co2, na.rm = TRUE)) %>% pull(co2)
+    maxco22020 <- climate_data %>% filter(year == 2020) %>% filter(co2 == max(co2, na.rm = TRUE)) %>% pull(co2)
+    maxco2year <- climate_data %>% filter(co2 == max(co2, na.rm = TRUE)) %>% pull(year)
+    maxco2 <- climate_data %>% filter(co2 == max(co2, na.rm = TRUE)) %>% pull(co2)
     maxPerCap <- climate_data %>% filter(year == 2020) %>% filter(co2_per_capita == max(co2_per_capita, na.rm = TRUE)) %>% pull(co2_per_capita)
     maxPerCapCountry <- climate_data %>% filter(year == 2020) %>% filter(co2_per_capita == max(co2_per_capita, na.rm = TRUE)) %>% pull(country)
     minPerCap <- climate_data %>% filter(year == 2020) %>% filter(co2 == min(co2, na.rm = TRUE)) %>% pull(co2)
@@ -14,7 +17,7 @@ server <- function(input, output) {
     minco2 <- climate_data %>% filter(country == "World") %>% filter(co2 == min(co2, na.rm = TRUE)) %>% slice_min(1) %>% pull(co2)
     minco2Year <- climate_data %>% filter(country == "World") %>% filter(co2 == min(co2, na.rm = TRUE)) %>% slice_min(1) %>% pull(year)
     
-    paragraph <- paste0("Through analyzing the data, the total CO2 emissions from the world in 2020 were ", maxco2, " million tonnes. The country with the highest CO2 emissions per capita in 2020 was ", 
+    paragraph <- paste0("Through analyzing the data, the total CO2 emissions from the world in 2020 were ", maxco22020, " million tonnes. However, ", maxco2year, " was the year with the highest emissions, sitting at ", maxco2, " million tonnes. The country with the highest CO2 emissions per capita in 2020 was ", 
                         maxPerCapCountry, ", with ", maxPerCap, " million tonnes emitted per capita. The country with the lowest CO2 emissions per capita was ", minPerCapCountry, ", with ", minPerCap, 
                         " million tonnes emitted per capita. Finally, the year with the lowest emissions was ", minco2Year, ", with ", minco2, " million tonnes of CO2 emitted.", sep = "")
     return(paragraph)
@@ -22,15 +25,13 @@ server <- function(input, output) {
   
   output$climatePlot <- renderPlotly({
 
-# Allow user to filter by multiple countries
-  climate_data <- climate_data %>% filter(country == input$user_category)
+    df <- climate_data %>% filter(year >= 2000) %>% filter(country == input$user_input) %>% rename("CO2 Emissions" = co2, "Year" = year)
 
-    # Make a scatter plot of oil per capita over time by country
-    my_plot <- ggplot(data = climate_data) + geom_line(mapping = aes(x = year, y = oil_co2_per_capita, color = country))
+    co2plot <- df %>% ggplot(aes(x = Year)) + geom_bar(aes(y = `CO2 Emissions`), stat = "identity", fill = input$color_input) + labs(title = "CO2 Emissions from 2000 to 2020") +
+      theme(plot.title = element_text(hjust = 0.5)) + ylab("CO2 Emissions (million tonnes)")
 
-    # Make interactive plot
-    my_plotly_plot <- ggplotly(my_plot)
+    co2plot <- ggplotly(co2plot)
 
-    return(my_plotly_plot)
+    return(co2plot)
   })
 }
--- my_ui.R
+++ my_ui.R
@@ -8,13 +8,15 @@ intro_tab <- tabPanel(
   "Introduction",
   fluidPage(theme = bs_theme(primary = "#091F43", font_scale = NULL, bootswatch = "cerulean"),
     h1("Introduction"),
-    p("In this project, I've chosen to analyze ___ in my Shiny app to ____"), ##############################
+    p("In this project, I've chosen to analyze carbon emissions from 2000 to 2020 in my Shiny app to better understand current trends related to global warming and what countries are the largest contributors to it."), ##############################
     h3("Who collected the data?"),
     p("The data was collected by Our World in Data, an online publication that focuses on large problems affecting the world, such as poverty, disease, and climate change."),
     h3("How was the data collected or generated?"),
-    p("The data is collected from four main sources: specialized institutes, research articles, international institutions/statistical agencies, and official government sources. Some examples of these sources would be UN institutions, the Peace Research Institute Oslo, and the World Bank."),
+    p("The data is collected from four main sources: specialized institutes, research articles, international institutions/statistical agencies, and official government sources. 
+      Some examples of these sources would be UN institutions, the Peace Research Institute Oslo, and the World Bank."),
     h3("Why was the data collected?"),
-    p("The data was collected to make use of preexisting research and data in an accessible way so that it progress can be made on the world's largest problems. A few issues that OWID seeks to solve are paywalls, jargon, and outdated data."),
+    p("The data was collected to make use of preexisting research and data in an accessible way so that it progress can be made on the world's largest problems. A few issues that 
+      OWID seeks to solve are paywalls, jargon, and outdated data."),
     h3("What are possible limitations or problems with this data?"),
     p("The data contains many NA values, limiting the conclusions that can be drawn from it. Because of this, I have to be aware of when assumptions are made when cleaning and analyzing the data.
         As a result of being removed from the collection of the data, I need to make sure that I don't suppress voices or exclude key information just to emphasize clarity and cleanliness in my visualizations.
@@ -22,13 +24,10 @@ intro_tab <- tabPanel(
     textOutput(outputId = "calcValues")
   )
 )
+
 plot_sidebar <- sidebarPanel(
-  selectInput(
-    inputId = "user_category",
-    label = "Select Country",
-    choices =  climate_data$country,
-    selected = "United States",
-    multiple = TRUE)
+  selectInput("user_input", "Select Region to Plot", choices = climate_data$country),
+  textInput("color_input", "Choose a Color (Hexcode)", value = "#55ffcc")
 )
 
 plot_main <- mainPanel(
@@ -40,13 +39,17 @@ plot_tab <- tabPanel(
   sidebarLayout(
     plot_sidebar,
     plot_main
-  )
+  ),
+  p("This chart was included to explore recent trends in CO2 emissions throughout the world. Some patterns that emerged from the chart were: 
+    China's CO2 emissions have more than tripled in the past 20 years, going from 3,349 to 10,667 million tonnes. On the other hand, North America's emissions have decreased from 7,107 in 2000 to 5,775 million tonnes in 2020.
+    Finally, the world's emissions have mostly increased in the past 20 years, though it saw a decrease from 2019 to 2020, likely due to Coronavirus quarantines reducing individual carbon emissions.")
 )
 
 value_tab <- tabPanel(
   "Value Sensitive Design",
   fluidPage(theme = bs_theme(primary = "#091F43", font_scale = NULL, bootswatch = "cerulean"),
-    h3("Prompt: Envision your system in use by a single stakeholder. Now imagine 100 such individuals interacting with the system. Then 1,000 individuals. Then 100,000. What new interactions might emerge from widespread use? Find three synergistic benefits of widespread use and three potential breakdowns."),
+    h3("Prompt: Envision your system in use by a single stakeholder. Now imagine 100 such individuals interacting with the system. Then 1,000 individuals. Then 100,000. What new 
+       interactions might emerge from widespread use? Find three synergistic benefits of widespread use and three potential breakdowns."),
     p("")
   )
 )
